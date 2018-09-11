db7f3a7ff69863b21fe121b1d37e531c36f87142
Randy Herasme <randyherasme6@gmail.com>
2018-09-11 16:50:39 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,43 @@
+def using_push (arr, elem)
+  arr.push(elem)
+end
+
+def using_unshift(arr,elem)
+  arr.unshift(elem)
+end
+
+def using_pop(arr)
+  arr.pop 
+end
+
+def pop_with_args(arr)
+  arr.pop(2)
+end
+
+def using_shift(arr)
+  arr.shift 
+end
+
+def shift_with_args(arr)
+  arr.shift(2)
+end
+
+def using_concat(arr1,arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(arr,elem)
+  arr.insert(4,elem)
+end
+
+def using_uniq(arr)
+  arr.uniq 
+end
+
+def using_flatten(arr)
+  arr.flatten 
+end
+
+
+def using_flatten (arr)
+  arr.flatten 
