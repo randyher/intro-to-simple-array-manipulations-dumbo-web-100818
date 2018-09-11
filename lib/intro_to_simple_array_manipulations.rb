c13fe840aa40c3ecf8851f41e85b5383b607e10e
Randy Herasme <randyherasme6@gmail.com>
2018-09-11 16:54:40 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,48 @@
+def using_push (arr, elem)
+  arr.push(elem)
+end
+
+def using_unshift(arr,elem)
+  arr.unshift(elem)
+end
+
+def using_pop(arr)
+  arr.pop 
+end
+
+def pop_with_args(arr)
+  arr.pop(2)
+end
+
+def using_shift(arr)
+  arr.shift 
+end
+
+def shift_with_args(arr)
+  arr.shift(2)
+end
+
+def using_concat(arr1,arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(arr,elem)
+  arr.insert(4,elem)
+end
+
+def using_uniq(arr)
+  arr.uniq 
+end
+
+def using_flatten(arr)
+  arr.flatten 
+end
+
+def using_delete(arr, elem)
+  arr.delete(elem)
+end
+
+def using_delete_at(arr,i)
+  arr.delete_at(i)
+end
+
